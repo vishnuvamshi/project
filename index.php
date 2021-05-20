aa3d947fe15b71d54788ffa1912a9eb77a024583
vishnuvamshi <84450560+vishnuvamshi@users.noreply.github.com>
2021-05-20 11:30:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-  $s="remotemysql.com:3306";
-	$un="0Qc34rspqi";
-	$pw="dGdmOsAXAn";
-	$db="0Qc34rspqi";
-	$con=mysqli_connect($s,$un,$pw,$db);
-  echo "<script>alert('welcome');location.href='sqldatabase.php'</script>";
+//   $s="remotemysql.com:3306";
+// 	$un="0Qc34rspqi";
+// 	$pw="dGdmOsAXAn";
+// 	$db="0Qc34rspqi";
+// 	$con=mysqli_connect($s,$un,$pw,$db);
+ // echo "<script>alert('welcome');location.href='sqldatabase.php'</script>";
+	echo "<script>location.href='sqldatabase.php'</script>";
 ?>
