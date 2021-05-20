b55c360cef232c26199ca78685e3557a2a7f5495
vishnuvamshi <84450560+vishnuvamshi@users.noreply.github.com>
2021-05-20 20:14:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 // $s="DROP TABLE customer,restaurant,items,ordered";
 // if(mysqli_query( $con,$s))
 // 	echo "<script>alert('droped');</script>";
- echo "<script>alert('welcome');location.href='sqldatabase.php'</script>";
+//  echo "<script>alert('welcome');location.href='sqldatabase.php'</script>";
 //to create database tables
 //echo "<script>location.href='sqldatabase.php'</script>";
 	echo "<script>location.href='menu.php'</script>";
