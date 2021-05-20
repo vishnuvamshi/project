bd293dbb071e73000c76b870c5f49898398397fc
vishnuvamshi <84450560+vishnuvamshi@users.noreply.github.com>
2021-05-20 20:11:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	$pw="dGdmOsAXAn";
 	$db="0Qc34rspqi";
 	$con=mysqli_connect($s,$un,$pw,$db);
-$s="DROP TABLE 'customer','restaurant','items','ordered'";
+$s="DROP TABLE customer,restaurant,items,ordered";
 if(mysqli_query( $con,$s))
 	echo "<script>alert('droped');</script>";
  // echo "<script>alert('welcome');location.href='sqldatabase.php'</script>";
