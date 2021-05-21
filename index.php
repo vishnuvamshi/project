a57ddd48174a9d7b01d31ad83d607458cd452693
vishnuvamshi <84450560+vishnuvamshi@users.noreply.github.com>
2021-05-21 21:21:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 	$pw="dGdmOsAXAn";
 	$db="0Qc34rspqi";
 	$con=mysqli_connect($s,$un,$pw,$db);
+$ss="drop table ordered;
+if(mysqli_query( $con,$ss))
+	echo "<script>alert('ordered droped');</script>";
+$sql4="CREATE table ordered(name varchar(15),mob varchar(11),restaurant varchar(30),food varchar(50),status varchar(2))";
+if(mysqli_query( $con,$sql4))
+	echo "<script>alert('ordered created');</script>";
 // $s="DROP TABLE customer,restaurant,items,ordered";
 // if(mysqli_query( $con,$s))
 // 	echo "<script>alert('droped');</script>";
