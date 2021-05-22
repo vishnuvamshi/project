4c7c1a686e57deac90a07a57da894348f6dbe578
vishnuvamshi <84450560+vishnuvamshi@users.noreply.github.com>
2021-05-22 13:50:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 	$pw="dGdmOsAXAn";
 	$db="0Qc34rspqi";
 	$con=mysqli_connect($s,$un,$pw,$db);
-$ss="drop table restaurant";
-if(mysqli_query( $con,$ss))
-	echo "<script>alert('restaurant droped');</script>";
-$sql1="CREATE table restaurant(name varchar(15),pwd varchar(15),mob varchar(11),rest varchar(30), PRIMARY KEY(name,pwd,mob), UNIQUE(rest),UNIQUE(mob))";
-		 if(mysqli_query( $con,$sql1)){  
-	    	echo "<script>alert('Restaurant table created successfully.');</script>";}
-	      else
-	      	echo "<script>alert('Restaurant table already created.');</script>";
+//$ss="drop table restaurant";
+//if(mysqli_query( $con,$ss))
+	//echo "<script>alert('restaurant droped');</script>";
+//$sql1="CREATE table restaurant(name varchar(15),pwd varchar(15),mob varchar(11),rest varchar(30), PRIMARY KEY(name,pwd,mob), UNIQUE(rest),UNIQUE(mob))";
+		// if(mysqli_query( $con,$sql1)){  
+	    	//echo "<script>alert('Restaurant table created successfully.');</script>";}
+	     // else
+	      	//echo "<script>alert('Restaurant table already created.');</script>";
 // $sql4="CREATE table ordered(name varchar(15),mob varchar(11),restaurant varchar(30),food varchar(50),status varchar(2))";
 // if(mysqli_query( $con,$sql4))
 // 	echo "<script>alert('ordered created');</script>";
