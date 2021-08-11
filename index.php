f6c91ae48e28313fa86160f54315616d1a77df2d
vishnuvamshi <84450560+vishnuvamshi@users.noreply.github.com>
2021-08-11 16:04:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,22 @@
 	$pw="dGdmOsAXAn";
 	$db="0Qc34rspqi";
 	$con=mysqli_connect($s,$un,$pw,$db);
+$s="select * from customer";
+$res=$con->query($s);
+		$rowsno=$res->num_rows;
+		if($res->num_rows>0)
+		{
+			while($row=$res->fetch_assoc())
+			{
+				echo "name:".$row['name']."pass".$row['pwd'];
+			}
+		}
 //$ss="drop table restaurant";
 //if(mysqli_query( $con,$ss))
 	//echo "<script>alert('restaurant droped');</script>";
 //$sql1="CREATE table restaurant(name varchar(15),pwd varchar(15),mob varchar(11),rest varchar(30), PRIMARY KEY(name,pwd,mob), UNIQUE(rest),UNIQUE(mob))";
-		// if(mysqli_query( $con,$sql1)){  
+		//if(mysqli_query( $con,$sql1)){ 
+		
 	    	//echo "<script>alert('Restaurant table created successfully.');</script>";}
 	     // else
 	      	//echo "<script>alert('Restaurant table already created.');</script>";
@@ -21,5 +32,5 @@
 //  echo "<script>alert('welcome');location.href='sqldatabase.php'</script>";
 //to create database tables
 //echo "<script>location.href='sqldatabase.php'</script>";
-	echo "<script>location.href='menu.php'</script>";
+	//echo "<script>location.href='menu.php'</script>";
 ?>
